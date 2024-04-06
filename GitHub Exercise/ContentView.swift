d06ceb8e8ef925da796f556477065d8e9f8f4834
Herbert Salov <h.salov@gmail.com>
2024-04-06 18:31:44 +0200

    Version 1.0

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -1,7 +1,6 @@
 //
 //  ContentView.swift
 //  GitHub Exercise
-//
 //  Created by Admin on 06.04.2024.
 //
 
@@ -10,12 +9,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+        .foregroundColor(.red)
     }
 }
 
